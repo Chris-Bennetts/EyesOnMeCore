d94bc984997237501bacf8f34fdaf1e9c501fc91
Chris Bennetts <Christopher.Mac.Bennetts@Gmail.com>
2023-04-10 17:59:26 +0100

    problems with browser scanner

--- EyesOnMeCore/wwwroot/js/BrowserScan.js
+++ EyesOnMeCore/wwwroot/js/BrowserScan.js
@@ -1,22 +1,63 @@
-﻿function urlExists(url, cb) {
-    jQuery.ajax({
-        url: url,
-        dataType: 'text',
-        type: 'GET',
-        complete: function (xhr) {
-            if (typeof cb === 'function')
-                cb.apply(this, [xhr.status]);
-        }
-    });
-}
+﻿//let pluginDictionary = {
+//    "gighmmpiobklfepjocnamgkkbiglidom": { Name: "Adblock", Address: "gdprrep@adblock.org"},
+//    "nemmanchfojaehgkbgcfmdiidbopakpp": { Name: "Virtru", Address: "info@virtru.com", Gender: "Female" },
+//};
 
-function browserscan() { 
-urlExists('EXTENTIONURLHERE'.function(status) {
-    if(status === 200) {
-    console.log("extension exists");
-} else if status === 400 {
-    console.log("extension does not exist");
-} else {
-    console.log("extension check error")
-    });
-}
+
+////function urlExists(url, cb) {
+////    jQuery.ajax({
+////        url: url,
+////        dataType: 'text',
+////        type: 'GET',
+////        complete: function (xhr) {
+////            if (typeof cb === 'function')
+////                cb.apply(this, [xhr.status]);
+////        }
+////    });
+////}
+
+////function browserscan(extentionurl) { 
+////    urlExists(extentionurl.function(status) {
+////    if(status === 200) {
+////    console.log("extension exists");
+////} else if status === 400 {
+////    console.log("extension does not exist");
+////} else {
+////    console.log("extension check error")
+////    });
+////}
+
+//function runfullscan() {
+//    for (const key of Object.keys(pluginDictionary)) {
+//        console.log(key + ": ");
+//        console.log(pluginDictionary[key]);
+//        browserscan(pluginDictionary[key])
+//    };
+//}
+
+
+
+
+//function UrlExists(url, cb) {
+//    jQuery.ajax({
+//        url:
+//            url,
+//        dataType: 'text',
+//        type:
+//            'GET',
+//        //complete: function (xhr) {
+//            if (typeof cb === 'function')
+//                cb.apply(this, [xhr.status]);
+//        }
+//    });
+//}
+//UrlExists('chrome-extension://nemmanchfojaehgkbgcfmdiidbopakpp/js/virtru-content.js', function (status) {
+//});
+//if (status = 200) {
+//==
+//        console.log("Virtru Exists!");
+//} else if (status === 404) {
+//    console.log("No Virtru :(");
+//} else {
+//}
+//console.log("Something is wrong.");
